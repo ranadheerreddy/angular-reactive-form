9a8faeb429706a1addddf8dd4b15576abfd1ed9d
ranadheerreddy <ranadheerreddy904@gmail.com>
2020-02-07 09:26:06 +0530

    latest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule, FormGroup, FormControl} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeroFormComponent } from './hero-form/hero-form.component';
+
 import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroFormComponent,
     ReactiveFormsComponent,
   ],
   imports: [
